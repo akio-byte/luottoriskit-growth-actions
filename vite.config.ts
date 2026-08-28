ab20a233fade2de5f0a7caadef1eefceb19f757d
laplandailab <wiktosound@gmail.com>
2026-08-28 15:18:23 +0300

    chore: prepare private Sites deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { cloudflare } from '@cloudflare/vite-plugin'
+import { sites } from '@openai/sites-vite-plugin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sites(), cloudflare()],
 })
